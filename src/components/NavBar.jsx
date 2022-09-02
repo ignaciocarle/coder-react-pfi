3712fb927a055fc6e88a1329a37a80d58bfd24ec
Ignacio Carle <ignaciocarle@gmail.com>
2022-09-01 21:54:09 -0300

    ⏩header estilado, listo para segunda entrega

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,13 +6,21 @@ import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 const Header = () => {
   return (
     <header style={styles.header}>
-      <img src={logo} alt="logo" />
-      <h1>Punto de Apoyo</h1>
+      <div style={styles.brand}>
+        <img src={logo} alt="logo" style={styles.logo} />
+        <h1>Punto de Apoyo</h1>
+      </div>
 
       <nav>
-        <a href="">Home</a>
-        <a href="">Productos</a>
-        <a href="">Nosotros</a>
+        <a href="" style={styles.links}>
+          Home
+        </a>
+        <a href="" style={styles.links}>
+          Productos
+        </a>
+        <a href="" style={styles.links}>
+          Nosotros
+        </a>
       </nav>
 
       <a href="">
@@ -25,10 +33,24 @@ const Header = () => {
 const styles = {
   header: {
     display: "flex",
+    flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    padding: "0 10%",
+    padding: "1.5rem 3rem",
     borderBottom: "solid 1px black",
+  },
+  brand: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: "1rem",
+  },
+  logo: {
+    width: 60,
+    height: 60,
+  },
+  links: {
+    padding: "0 0.5rem",
   },
 };
 
